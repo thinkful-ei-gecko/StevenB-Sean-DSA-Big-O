29b36fc670aae465a7d7c6696b9c108ce15d15ac
Steven Bull <stevenwbulljr@gmail.com>
2019-11-05 19:13:29 +0000

    finished problems 1-2 iteratively

--- index.js
+++ index.js
@@ -9,4 +9,26 @@ const towerOfHanoi = (discs, startPeg, endPeg, middlePeg) => {
   return;
 };
 
-towerOfHanoi(2, '1', '3', '2', 0);
+//towerOfHanoi(7, '1', '3', '2', 0);
+
+// solving recursive problems 1-7 iteratively
+
+// 1.
+
+const sheepCounter = (numOfSheep) => {
+  for(let i = numOfSheep; i >= 1; i--) 
+    console.log(`${i}: Another sheep jumps over the fence`);
+
+  console.log('All the sheep jumped over the fence');
+};
+
+//sheepCounter(3);
+
+const powerCalculator = (base, exponent) => {
+  if (exponent < 0)
+    return 'exponent should be >= 0';
+  return base ** (exponent);
+};
+
+//console.log(powerCalculator(10,2));
+//console.log(powerCalculator(10,-2));
